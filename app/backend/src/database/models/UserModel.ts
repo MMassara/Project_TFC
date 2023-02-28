04dc31e9e86579f83bb7df852a9d79755934031d
Matheus Massara <matheus.massara@hotmail.com>
2023-02-28 16:10:41 -0300

    Requisito 06

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,40 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class UserModel extends Model {
+  declare readonly id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  modelName: 'users',
+});
+
+export default UserModel;
